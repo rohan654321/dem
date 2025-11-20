be064a83f6d09ba47cb4d3d2324e1060ff8c89c2
rohan654321 <mondalrohan201@gmail.com>
2025-11-20 18:04:47 +0530

    update

--- components/UI/LeadForm.jsx
+++ components/UI/LeadForm.jsx
@@ -5,6 +5,48 @@ import { useState, useEffect } from 'react';
 import Button from './Button';
 import { useRouter } from 'next/navigation';
 
+// Car models data organized by brand
+const carModelsByBrand = {
+  'AMC': ['Eagle', 'Gremlin', 'Hornet', 'Javelin', 'Pacer', 'Rebel'],
+  'Acura': ['CL', 'ILX', 'Integra', 'MDX', 'NSX', 'RDX', 'RL', 'RLX', 'RSX', 'TL', 'TLX', 'TSX'],
+  'Alfa Romeo': ['4C', 'Giulia', 'Stelvio', 'Spider'],
+  'Aston Martin': ['DB11', 'DB9', 'DBS', 'Rapide', 'Vantage', 'Vanquish'],
+  'Audi': ['A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'Q3', 'Q5', 'Q7', 'Q8', 'R8', 'RS3', 'RS5', 'RS7', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'TT'],
+  'Bentley': ['Bentayga', 'Continental', 'Flying Spur', 'Mulsanne'],
+  'BMW': ['1 Series', '2 Series', '3 Series', '4 Series', '5 Series', '6 Series', '7 Series', '8 Series', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'Z4', 'i3', 'i8'],
+  'Buick': ['Cascada', 'Enclave', 'Encore', 'Envision', 'LaCrosse', 'Regal', 'Verano'],
+  'Cadillac': ['ATS', 'CT4', 'CT5', 'CT6', 'CTS', 'DTS', 'ELR', 'Escalade', 'SRX', 'XT4', 'XT5', 'XT6', 'XTS'],
+  'Chevrolet': ['Blazer', 'Camaro', 'Captiva', 'Colorado', 'Corvette', 'Cruze', 'Equinox', 'Express', 'Impala', 'Malibu', 'Silverado', 'Sonic', 'Spark', 'Suburban', 'Tahoe', 'Traverse', 'Trax', 'Volt'],
+  'Chrysler': ['200', '300', 'Pacifica', 'Town & Country', 'Voyager'],
+  'Dodge': ['Avenger', 'Challenger', 'Charger', 'Dart', 'Durango', 'Grand Caravan', 'Journey', 'Neon', 'Nitro', 'Ram', 'Viper'],
+  'Fiat': ['124 Spider', '500', '500L', '500X', '500e'],
+  'Ford': ['Bronco', 'EcoSport', 'Edge', 'Escape', 'Expedition', 'Explorer', 'F-150', 'Fiesta', 'Flex', 'Focus', 'Fusion', 'GT', 'Mustang', 'Ranger', 'Taurus', 'Transit'],
+  'GMC': ['Acadia', 'Canyon', 'Sierra', 'Terrain', 'Yukon'],
+  'Honda': ['Accord', 'Civic', 'Clarity', 'CR-V', 'CR-Z', 'Element', 'Fit', 'HR-V', 'Insight', 'Odyssey', 'Passport', 'Pilot', 'Ridgeline'],
+  'Hyundai': ['Accent', 'Azera', 'Elantra', 'Genesis', 'Ioniq', 'Kona', 'Palisade', 'Santa Fe', 'Sonata', 'Tucson', 'Veloster', 'Venue'],
+  'Infiniti': ['EX', 'FX', 'G', 'I', 'JX', 'M', 'Q', 'QX', 'QX50', 'QX60', 'QX80'],
+  'Jaguar': ['E-PACE', 'F-PACE', 'F-TYPE', 'I-PACE', 'XE', 'XF', 'XJ', 'XK'],
+  'Jeep': ['Cherokee', 'Compass', 'Gladiator', 'Grand Cherokee', 'Liberty', 'Patriot', 'Renegade', 'Wrangler'],
+  'Kia': ['Cadenza', 'Forte', 'K5', 'K900', 'Niro', 'Optima', 'Rio', 'Sedona', 'Seltos', 'Sorento', 'Soul', 'Sportage', 'Stinger', 'Telluride'],
+  'Land Rover': ['Defender', 'Discovery', 'Discovery Sport', 'Freelander', 'Range Rover', 'Range Rover Evoque', 'Range Rover Sport', 'Range Rover Velar'],
+  'Lexus': ['CT', 'ES', 'GS', 'GX', 'HS', 'IS', 'LC', 'LFA', 'LS', 'LX', 'NX', 'RC', 'RX', 'SC', 'UX'],
+  'Lincoln': ['Aviator', 'Continental', 'Corsair', 'MKC', 'MKS', 'MKT', 'MKX', 'MKZ', 'Nautilus', 'Navigator'],
+  'Mazda': ['2', '3', '5', '6', 'CX-3', 'CX-30', 'CX-5', 'CX-7', 'CX-9', 'MX-5 Miata', 'RX-8'],
+  'Mercedes': ['A-Class', 'B-Class', 'C-Class', 'CLA', 'CLS', 'E-Class', 'G-Class', 'GLA', 'GLB', 'GLC', 'GLE', 'GLK', 'GLS', 'M-Class', 'S-Class', 'SL', 'SLC', 'SLK', 'SLS'],
+  'Mini': ['Clubman', 'Convertible', 'Countryman', 'Coupe', 'Hardtop', 'Paceman', 'Roadster'],
+  'Mitsubishi': ['Eclipse', 'Eclipse Cross', 'Endeavor', 'Galant', 'i-MiEV', 'Lancer', 'Mirage', 'Outlander', 'Outlander Sport', 'Raider'],
+  'Nissan': ['370Z', 'Altima', 'Armada', 'Frontier', 'GT-R', 'Juke', 'Kicks', 'Leaf', 'Maxima', 'Murano', 'Navara', 'Pathfinder', 'Quest', 'Rogue', 'Sentra', 'Titan', 'Versa', 'X-Trail', 'Xterra'],
+  'Porsche': ['911', 'Boxster', 'Cayenne', 'Cayman', 'Macan', 'Panamera', 'Taycan'],
+  'RAM': ['1500', '2500', '3500', 'Promaster', 'Promaster City'],
+  'Subaru': ['Ascent', 'BRZ', 'Crosstrek', 'Forester', 'Impreza', 'Legacy', 'Outback', 'STI', 'SVX', 'Tribeca', 'WRX', 'XV'],
+  'Tesla': ['Model 3', 'Model S', 'Model X', 'Model Y', 'Cybertruck', 'Roadster'],
+  'Toyota': ['4Runner', '86', 'Avalon', 'C-HR', 'Camry', 'Corolla', 'FJ Cruiser', 'Highlander', 'Land Cruiser', 'Mirai', 'Prius', 'RAV4', 'Sequoia', 'Sienna', 'Supra', 'Tacoma', 'Tundra', 'Venza', 'Yaris'],
+  'Volkswagen': ['Arteon', 'Atlas', 'Beetle', 'CC', 'Eos', 'Golf', 'GTI', 'Jetta', 'Passat', 'Phaeton', 'Tiguan', 'Touareg'],
+  'Volvo': ['C30', 'C70', 'S40', 'S60', 'S80', 'S90', 'V40', 'V50', 'V60', 'V70', 'V90', 'XC40', 'XC60', 'XC70', 'XC90'],
+  'Ferrari': ['California', 'Enzo', 'F8', 'Portofino', 'Roma', 'SF90', 'Testarossa'],
+  'Lamborghini': ['Aventador', 'Gallardo', 'Huracan', 'Murcielago', 'Urus']
+};
+
 export default function LeadForm({ prefilledBrand = '' }) {
   const router = useRouter();
   const [formData, setFormData] = useState({
@@ -23,6 +65,7 @@ export default function LeadForm({ prefilledBrand = '' }) {
   
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submitMessage, setSubmitMessage] = useState('');
+  const [availableModels, setAvailableModels] = useState([]);
 
   // Update form when prefilledBrand changes
   useEffect(() => {
@@ -31,8 +74,24 @@ export default function LeadForm({ prefilledBrand = '' }) {
         ...prev,
         car: prefilledBrand
       }));
+      // Set available models when brand is prefilled
+      setAvailableModels(carModelsByBrand[prefilledBrand] || []);
     }
   }, [prefilledBrand]);
+
+  // Update available models when car brand changes
+  useEffect(() => {
+    if (formData.car && carModelsByBrand[formData.car]) {
+      setAvailableModels(carModelsByBrand[formData.car]);
+      // Reset model when brand changes
+      setFormData(prev => ({
+        ...prev,
+        model: ''
+      }));
+    } else {
+      setAvailableModels([]);
+    }
+  }, [formData.car]);
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -124,56 +183,27 @@ export default function LeadForm({ prefilledBrand = '' }) {
             required
           >
             <option value="">Choose Your Car</option>
-            <option value="AMC">AMC</option>
-            <option value="Acura">Acura</option>
-            <option value="Alfa">Alfa</option>
-            <option value="Aston Martin">Aston Martin</option>
-            <option value="Audi">Audi</option>
-            <option value="Bentley">Bentley</option>
-            <option value="BMW">BMW</option>
-            <option value="Buick">Buick</option>
-            <option value="Cadillac">Cadillac</option>
-            <option value="Chevy">Chevy</option>
-            <option value="Chrysler">Chrysler</option>
-            <option value="Dodge">Dodge</option>
-            <option value="Fiat">Fiat</option>
-            <option value="Ford">Ford</option>
-            <option value="GMC">GMC</option>
-            <option value="Honda">Honda</option>
-            <option value="Hyundai">Hyundai</option>
-            <option value="Infiniti">Infiniti</option>
-            <option value="Jaguar">Jaguar</option>
-            <option value="Jeep">Jeep</option>
-            <option value="Kia">Kia</option>
-            <option value="Landrover">Landrover</option>
-            <option value="Lexus">Lexus</option>
-            <option value="Lincoln">Lincoln</option>
-            <option value="Mazda">Mazda</option>
-            <option value="Mercedes">Mercedes</option>
-            <option value="Mini">Mini</option>
-            <option value="Mitsubishi">Mitsubishi</option>
-            <option value="Nissan">Nissan</option>
-            <option value="Porsche">Porsche</option>
-            <option value="RAM">RAM</option>
-            <option value="Subaru">Subaru</option>
-            <option value="Tesla">Tesla</option>
-            <option value="Toyota">Toyota</option>
-            <option value="Volkswagen">Volkswagen</option>
-            <option value="Volvo">Volvo</option>
+            {Object.keys(carModelsByBrand).map(brand => (
+              <option key={brand} value={brand}>{brand}</option>
+            ))}
           </select>
         </div>
 
         {/* Row 2 - Model & Part Type */}
         <div className="grid grid-cols-2 gap-3">
-          <input
-            type="text"
+          <select
             name="model"
-            placeholder="Car Model"
             value={formData.model}
             onChange={handleChange}
             className="w-full px-3 py-2 border border-gray-300 rounded text-gray-700 focus:ring-2 focus:ring-red-500 focus:border-transparent text-sm"
             required
-          />
+            disabled={!formData.car}
+          >
+            <option value="">{formData.car ? `Select ${formData.car} Model` : 'Select Car First'}</option>
+            {availableModels.map(model => (
+              <option key={model} value={model}>{model}</option>
+            ))}
+          </select>
 
           <select
             name="part"
